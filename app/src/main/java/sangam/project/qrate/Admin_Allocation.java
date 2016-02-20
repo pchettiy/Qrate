fd5559ec03e519999ec91c1e5f6bc5e143fcc680
rbkrishna <krishnarb3@gmail.com>
2016-02-20 11:04:23 +0530

    Added Admin Allocation Activity

--- app/src/main/java/sangam/project/qrate/Admin_Allocation.java
+++ app/src/main/java/sangam/project/qrate/Admin_Allocation.java
@@ -1,13 +1,148 @@
 package sangam.project.qrate;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Admin_Allocation extends AppCompatActivity {
+
+    List<String> levelList;
+    List<String> resourceKindList;
+    Spinner spinnerLevel;
+    Spinner spinnerResouceKind;
+    EditText editTextDuration;
+    RatingBar ratingBar;
+    Button buttonSubmit;
+    String track="";
+    String url="";
+    String level="",resourceKind="";
+    String rating="";
+    ProgressDialog pd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin__allocation);
+        levelList = new ArrayList<>(Arrays.asList("Beginner","Intermediate","Advanced"));
+        resourceKindList = new ArrayList<>(Arrays.asList("Video","Course","WebTutorial"));
+        spinnerLevel = (Spinner)findViewById(R.id.spinner3);
+        spinnerResouceKind = (Spinner)findViewById(R.id.spinner4);
+        editTextDuration = (EditText)findViewById(R.id.editText2);
+        ratingBar = (RatingBar)findViewById(R.id.ratingBar);
+        buttonSubmit = (Button)findViewById(R.id.button5);
+        Bundle bundle = getIntent().getExtras();
+        track = bundle.getString("track");
+        url = bundle.getString("url");
+        ArrayAdapter adapterLevel = new ArrayAdapter(this,R.layout.support_simple_spinner_dropdown_item,levelList);
+        ArrayAdapter adapterResourceList = new ArrayAdapter(this,R.layout.support_simple_spinner_dropdown_item,resourceKindList);
+        spinnerLevel.setAdapter(adapterLevel);
+        spinnerResouceKind.setAdapter(adapterResourceList);
+        spinnerLevel.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                level = levelList.get(i);
+            }
+        });
+        spinnerResouceKind.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                resourceKind = levelList.get(i);
+            }
+        });
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
+                rating = v+"";
+            }
+        });
+        buttonSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(level.equals("")||resourceKind.equals("")||rating.equals(""))
+                    Toast.makeText(Admin_Allocation.this,"Please select Properly",Toast.LENGTH_SHORT).show();
+                else {
+                    new AsyncSendAllocationTask().execute(editTextDuration.getText().toString());
+                    //new Async
+                }
+            }
+        });
+    }
+
+    public class AsyncSendAllocationTask extends AsyncTask<String,Void,String>
+    {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pd = ProgressDialog.show(Admin_Allocation.this,"Sending","",true,false);
+        }
+        @Override
+        protected String doInBackground(String... strings) {
+            String result = null;
+            String downloadurl="https://spider.nitt.edu/~praba1110/qrate/storeapproved.php";
+            try{
+
+                URL url=new URL(downloadurl);
+                try{
+                    HttpURLConnection urlConnection= (HttpURLConnection) url.openConnection();
+                    String postParameters="track="+track.toLowerCase()+"&kind="+resourceKind.toLowerCase()
+                                    +"&min="+strings[0].toLowerCase()+"&url="+url+"&level="+level.toLowerCase()
+                                    +"&rating="+rating.toLowerCase();
+                    urlConnection.setRequestMethod("POST");
+                    urlConnection.setRequestProperty("Content-Type",
+                            "application/x-www-form-urlencoded");
+                    urlConnection.setFixedLengthStreamingMode(
+                            postParameters.getBytes().length);
+                    PrintWriter out = new PrintWriter(urlConnection.getOutputStream());
+                    out.print(postParameters);
+                    out.close();
+                    InputStream inputStream=urlConnection.getInputStream();
+                    BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(inputStream));
+                    String line="";
+
+                    while((line=bufferedReader.readLine())!=null){
+                        result+=line;
+                    }
+                    Log.d("DATA",result);
+                    inputStream.close();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            return result;
+        }
+        @Override
+        protected void onPostExecute(String mString) {
+            super.onPostExecute(mString);
+            Toast.makeText(Admin_Allocation.this,"Submitted Successfully",Toast.LENGTH_SHORT).show();
+            pd.dismiss();
+        }
+
     }
 }
--- app/src/main/java/sangam/project/qrate/Admin_check_topics.java
+++ app/src/main/java/sangam/project/qrate/Admin_check_topics.java
@@ -1,13 +1,15 @@
 package sangam.project.qrate;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.AppCompatDrawableManager;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,13 +18,27 @@ import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
-import java.util.zip.Inflater;
 
 public class Admin_check_topics extends AppCompatActivity {
 
+    ProgressDialog pd;
     ArrayList<AdminTopic> admintopicslist;
     String track;
+    ListView listView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +46,8 @@ public class Admin_check_topics extends AppCompatActivity {
         if(intent!=null){
             track=intent.getStringExtra("topic");
         }
+        pd = ProgressDialog.show(this,"Loading","",false,false);
+        new AsyncGetTitleAndUrl().execute(track);
         setContentView(R.layout.activity_admin_check_topics);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -42,11 +60,68 @@ public class Admin_check_topics extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-        ListView listView= (ListView) findViewById(R.id.listView3);
-        listView.setAdapter(new AdminAdapter(this,admintopicslist));
+        listView= (ListView) findViewById(R.id.listView3);
 
     }
 
+    public class AsyncGetTitleAndUrl extends AsyncTask<String,Void,String> {
+        @Override
+        protected String doInBackground(String... strings) {
+            String result = null;
+            String downloadurl="https://spider.nitt.edu/~praba1110/qrate/storeapproved.php";
+            try{
+
+                URL url=new URL(downloadurl);
+                try{
+                    HttpURLConnection urlConnection= (HttpURLConnection) url.openConnection();
+                    String postParameters="topic="+strings[0];
+                    urlConnection.setRequestMethod("POST");
+                    urlConnection.setRequestProperty("Content-Type",
+                            "application/x-www-form-urlencoded");
+                    urlConnection.setFixedLengthStreamingMode(
+                            postParameters.getBytes().length);
+                    PrintWriter out = new PrintWriter(urlConnection.getOutputStream());
+                    out.print(postParameters);
+                    out.close();
+                    InputStream inputStream=urlConnection.getInputStream();
+                    BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(inputStream));
+                    String line="";
+
+                    while((line=bufferedReader.readLine())!=null){
+                        result+=line;
+                    }
+                    Log.d("DATA",result);
+                    inputStream.close();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Log.d("Logging", s);
+            try {
+                JSONArray array = new JSONArray(s);
+                for(int i=0;i<array.length();i++) {
+                    JSONObject object = array.getJSONObject(i);
+                    AdminTopic adminTopic = new AdminTopic();
+                    adminTopic.title = object.getString("topic");
+                    adminTopic.url = object.getString("url");
+                    admintopicslist.add(adminTopic);
+                }
+            AdminAdapter adminAdapter = new AdminAdapter(Admin_check_topics.this,admintopicslist);
+            listView.setAdapter(adminAdapter);
+            }catch(JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
     public class AdminAdapter extends ArrayAdapter{
         Context context;
         ArrayList<AdminTopic> topics;
@@ -57,7 +132,7 @@ public class Admin_check_topics extends AppCompatActivity {
         }
 
         @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
+        public View getView(final int position, View convertView, ViewGroup parent) {
             LayoutInflater inflater= LayoutInflater.from(getContext());
             View v=inflater.inflate(R.layout.admin_topics_single_item,parent,false);
             TextView title= (TextView) v.findViewById(R.id.title_text);
@@ -66,7 +141,10 @@ public class Admin_check_topics extends AppCompatActivity {
             cardlayout.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-
+                    Intent intent = new Intent(Admin_check_topics.this,Admin_Allocation.class);
+                    intent.putExtra("track",track);
+                    intent.putExtra("url",admintopicslist.get(position).url);
+                    startActivity(intent);
                 }
             });
             return v;
